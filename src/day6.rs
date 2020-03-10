71257452595cd1f33d4e20d6b5834453dfe742c2
Itamar Gafni <itamar.gafni@imperva.com>
2020-03-10 17:53:23 +0200

    Set up default build and build before debug

--- src/day6.rs
+++ src/day6.rs
@@ -1,20 +1,21 @@
 use std::collections::HashMap;
 
-const EXAMPLE: &str = "COM)B
-B)C
-C)D
-D)E
-E)F
-B)G
-G)H
-D)I
-E)J
-J)K
-K)L";
+// const EXAMPLE: &str = "COM)B
+// B)C
+// C)D
+// D)E
+// E)F
+// B)G
+// G)H
+// D)I
+// E)J
+// J)K
+// K)L";
 
+#[derive(Debug)]
 struct Node {
-    name: String,
-    parent: Option<String>,
+    // name: String,
+    // parent: Option<String>,
     children: Vec<String>,
 }
 
@@ -23,6 +24,7 @@ const INTPUT_PATH: &str = "src/day6_input.txt";
 pub fn solve_day_6_pt_1() -> usize {
     let input = std::fs::read_to_string(INTPUT_PATH).unwrap();
     let tree = make_tree(&input);
+    println!("{:?}", tree);
     let mut count = 0;
     for node_name in tree.keys() {
         let node = &tree.get(node_name).unwrap();
@@ -37,19 +39,20 @@ fn make_tree(input: &str) -> HashMap<String, Node> {
         let objects = line.split(")").collect::<Vec<&str>>();
         let obj1 = objects[0].to_string();
         let obj2 = objects[1].to_string();
-        tree.entry(obj1.clone())
-            .or_insert(Node {
-                name: obj1.clone(),
-                parent: None,
-                children: vec![],
-            })
-            .children
-            .push(obj2.clone());
+        if obj1 == "5JZ" {
+            let a = "this";
+        }
+        tree.entry(obj1.clone()).or_insert(Node {
+            // name: obj1.clone(),
+            // parent: None,
+            children: vec![],
+        });
+        tree.get_mut(&obj1).unwrap().children.push(obj2.clone());
         tree.insert(
             obj2.clone(),
             Node {
-                name: obj2.clone(),
-                parent: Some(obj1.clone()),
+                // name: obj2.clone(),
+                // parent: Some(obj1.clone()),
                 children: vec![],
             },
         );
